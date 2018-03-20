6f4a16df598a8f00abd5769f0585b94774ad25d0
Julius Härtl <jus@bitgrid.net>
2018-03-20 10:41:01 +0100

    REMOVE: just one test for faster debugging
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- tests/ui-regression/run-tests.sh
+++ tests/ui-regression/run-tests.sh
@@ -8,7 +8,7 @@ done
 
 node writeConfig.js
 ./node_modules/.bin/mocha test/installSpec.js --reporter json --timeout 20000 > out/install.json
-./node_modules/.bin/mocha test/loginSpec.js --reporter json --timeout 20000 > out/login.json
-./node_modules/.bin/mocha test/settingsSpec.js --reporter json --timeout 20000 > out/settings.json
-./node_modules/.bin/mocha test/publicSpec.js --reporter json --timeout 20000 > out/public.json
+#./node_modules/.bin/mocha test/loginSpec.js --reporter json --timeout 20000 > out/login.json
+#./node_modules/.bin/mocha test/settingsSpec.js --reporter json --timeout 20000 > out/settings.json
+#./node_modules/.bin/mocha test/publicSpec.js --reporter json --timeout 20000 > out/public.json
 
